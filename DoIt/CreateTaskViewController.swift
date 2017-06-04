236ea4df4687c7b494cf7a70a61327ee1a18eb6a
sahaj makharia <sahajmakharia@ResNet-61-196.resnet.ucsb.edu>
2017-06-04 06:05:07 -0700

    Basic framework for the app: type of tasks, prioritising and adding tasks

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by sahaj makharia on 04/06/17.
+//  Copyright © 2017 Sahaj Makharia. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
---
+++ DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by sahaj makharia on 04/06/17.
+//  Copyright © 2017 Sahaj Makharia. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
